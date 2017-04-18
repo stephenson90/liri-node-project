8ed42a4a33613e07e56265779889c315a21e28fe
Jean Davilus <jsd6@njit.edu>
2017-04-17 20:24:06 -0400

    first push

---
+++ assets/javascript/keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<rxPdwo1AApJbJJ7PrFafXB2Th>',
+  consumer_secret: '< BURDVB9LTha4mbxG6LjQNyXKZ6S9ZMaKnWfJ7OrggEwoJgfxOv>',
+  access_token_key: '< 854091217948332032-Up4BMtPnRMSUwL48ZEemLI9jUhJApOh>',
+  access_token_secret: '< 6Yx0AgDubYXLKCqPyKevwQTltMAlNN8t6Di2BgXQX8N14>',
+}
+
